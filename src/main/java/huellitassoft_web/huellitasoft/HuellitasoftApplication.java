f59a8efbde55280d2de02925b3f680aa4312ca2a
W1llAn <wchimborazo2840@uta.edu.ec>
2025-10-15 11:45:42 -0500

    first commit

---
+++ src/main/java/huellitassoft_web/huellitasoft/HuellitasoftApplication.java
@@ -0,0 +1,13 @@
+package huellitassoft_web.huellitasoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuellitasoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuellitasoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/huellitassoft_web/huellitasoft/HuellitasoftApplicationTests.java
@@ -0,0 +1,13 @@
+package huellitassoft_web.huellitasoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuellitasoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
