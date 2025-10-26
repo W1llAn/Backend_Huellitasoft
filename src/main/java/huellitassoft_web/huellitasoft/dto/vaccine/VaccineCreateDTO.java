5065ca16f7fea22a55b94df77ed6e6882fb21718
Luis <lquishpe5771@uta.edu.ec>
2025-10-26 06:07:37 -0500

    feat: implementación de VaccineController con endpoints CRUD (listar, crear, actualizar, eliminar) y documentación con Swagger

---
+++ src/main/java/huellitassoft_web/huellitasoft/dto/vaccine/VaccineCreateDTO.java
@@ -0,0 +1,4 @@
+package huellitassoft_web.huellitasoft.dto.vaccine;
+
+public class VaccineCreateDTO {
+}
---
+++ src/main/java/huellitassoft_web/huellitasoft/service/VaccineService.java
@@ -0,0 +1,8 @@
+package huellitassoft_web.huellitasoft.service.impl;
+
+public interface VaccineService {
+    /*
+    * Obtener todas las vacunas
+    * return lista de todas las vacunas */
+     public
+}
