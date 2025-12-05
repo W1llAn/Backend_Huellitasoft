4ca30682857a0945820dec6999389f36fc2e781f
Luis <lquishpe5771@uta.edu.ec>
2025-12-05 12:01:58 -0500

    feat: se implemento el estado para la vacuna en la entidad

--- src/main/java/huellitassoft_web/huellitasoft/entity/PetVaccination.java
+++ src/main/java/huellitassoft_web/huellitasoft/entity/PetVaccination.java
@@ -1,6 +1,8 @@
 package huellitassoft_web.huellitasoft.entity;
 
 
+import huellitassoft_web.huellitasoft.enums.PetSchemeState;
+import huellitassoft_web.huellitasoft.enums.VaccinationPetState;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,6 +34,10 @@ public class PetVaccination {
     // Se setea desde backend (now), nunca desde el cliente
     @Column(name = "fecha_aplicada", nullable = false)
     private LocalDateTime fechaAplicada;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "estado", nullable = false, length = 15)
+    private VaccinationPetState estado = VaccinationPetState.ACTIVA;
 
     // Usuario (veterinario) que aplicó la vacuna
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
